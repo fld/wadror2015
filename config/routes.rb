a70419bfd97e8ef4cc244ec07fbc5c7dd51e8b4e
fld <fld@r00t3d.com>
2015-01-25 22:08:15 +0200

    week 2 complete.

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,25 @@ Rails.application.routes.draw do
 
   resources :breweries
 
+  resources :ratings, only: [:index, :new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'breweries#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to:'ratings#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # HTTP Post
+  #post 'ratings', to: 'ratings#create'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
